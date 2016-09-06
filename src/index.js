57666445113ea3de1488fac318d94fe91f8bde1a
zach <zbluedorn@medallia.com>
2016-09-06 14:43:24 -0700

    small corrections

--- src/index.js
+++ src/index.js
@@ -7,44 +7,49 @@ let config = {
 }
 firebase.initializeApp(config)
 
-
-
-
-
-
-
 // Create an array to store the video uploads
 let videos = []
 
+/*
+  VIDEO UPLOAD BUTTONS
+*/
+// Add first video to videos array once uploaded
+// (Note: Closure is used to pass video number as an argument to
+// the callback)
+document
+  .getElementById('videoInput1')
+  .addEventListener('change', addVideo(1))
 
+// Add second video to videos array
+document
+  .getElementById('videoInput2')
+  .addEventListener('change', addVideo(2))
 
-
-
-
+/*
+  NEXT & SUBMIT BUTTONS
+*/
 
 // Show second question when next button is clicked
-let nextButton1 = document.getElementById('nextButton1')
-nextButton1.addEventListener('click', showNextQuestion)
+document
+  .getElementById('nextButton1')
+  .addEventListener('click', showNextQuestion)
 
+// Submit videos to Firebase once submit button is clicked
+document
+  .getElementById('submitButton')
+  .addEventListener('click', handleFiles)
+
+
+
+/*
+  HELPER FUNCTIONS
+*/
 function showNextQuestion(event) {
   let questionPanel1 = document.getElementById('questionPanel1')
   questionPanel1.className = "mui-panel fadeOut"
   let questionPanel2 = document.getElementById('questionPanel2')
   questionPanel2.className = "mui-panel fadeIn"
 }
-
-
-
-
-
-
-
-
-// Add first video to videos array once uploaded
-// Closure is used to pass video number as an argument to
-// the callback
-let videoInput1 = document.getElementById('videoInput1')
-videoInput1.addEventListener('change', addVideo(1))
 
 function addVideo(number) {
   number = number.toString()
@@ -59,36 +64,18 @@ function addVideo(number) {
     videos.push(video)
 
     // Show video preview section
-    let videoGrid = document.getElementById('videoDiv' + number)
-    videoGrid.className = 'mdl-grid mui--text-center'
+    let videoDiv = document.getElementById('videoDiv' + number)
+    videoDiv.className = 'mdl-grid mui--text-center'
 
     // Set video url to uploaded video local URL
     let videoElement = document.getElementById('video' + number)
     // Create a local url for displaying the uploaded video
     let url = URL.createObjectURL(video)
+    console.log("video element is", videoElement)
+    console.log("setting url to", url)
     videoElement.setAttribute('src', url)
   }
 }
-
-
-
-
-
-
-
-// Add second video to videos array once uploaded
-let videoInput2 = document.getElementById('videoInput2')
-videoInput2.addEventListener('change', addVideo(2))
-
-
-
-
-
-
-
-// Submit videos to Firebase once submit button is clicked
-let submitButton = document.getElementById('submitButton')
-submitButton.addEventListener('click', handleFiles)
 
 function handleFiles() {
   // Hide last question panel
@@ -99,26 +86,28 @@ function handleFiles() {
   let successPanel = document.getElementById('successPanel')
   successPanel.className = "mdl-grid mui--text-center"
 
+  // Use a random folder name and update storage pointer
+  let userFolder = "User_" + Math.random().toString(36).substring(7)
+
   // Create firebase storage pointer
-  let storageRef = firebase.storage().ref()
+  let storageRef = firebase.storage().ref().child(userFolder)
 
   // Upload videos to firebase asynchronously
   videos.forEach( (video) => {
-    // Use a random folder name and update storage pointer
-    let path = "User_" + Math.random().toString(36).substring(7)
-    storageRef = storageRef.child(path + video.name)
+    storageRef = storageRef.child(video.name)
 
     storageRef
     // Upload video
     .put(video)
     // Display download URL on page
     .then( () => {
-      storageRef.getDownloadURL()
-        .then( (url) => {
-          let bodyUrl = document.getElementById('url')
-          bodyUrl.innerHTML += "File '" + video.name + "' submitted.<br><a href='" + url + "'>Download</a><br><br>"
-        })
-        .catch( (error) => console.log(error) )
+      storageRef
+      .getDownloadURL()
+      .then( (url) => {
+        let bodyUrl = document.getElementById('url')
+        bodyUrl.innerHTML += "File '" + video.name + "' submitted.<br><a href='" + url + "'>Download</a><br><br>"
+      })
+      .catch( (error) => console.log(error) )
     })
     .catch( (error) => console.log(error) )
   })
